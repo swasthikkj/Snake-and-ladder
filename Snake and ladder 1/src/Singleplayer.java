7fee9990d25d314bb7feb5c8ecee195bb3f1dc07
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-03-12 17:35:23 +0530

    program

---
+++ Snake and ladder 1/src/Singleplayer.java
@@ -0,0 +1,10 @@
+
+public class Singleplayer {
+	public static void main(String[] args) {
+		int playerposition = 0;
+		System.out.println("Welcome To Snake And Ladder Game");
+		System.out.println("Player 1 is in position " +playerposition);
+	}
+
+}
+
